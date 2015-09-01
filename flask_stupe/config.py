cf08198c292940590551180cddd9807a0050305c
Guillaume Gelin <ramnes@1000mercis.com>
2015-09-01 11:32:41 +0200

    Avoid `DEBUG=0` to be casted as `DEBUG=True`

--- flask_stupe/config.py
+++ flask_stupe/config.py
@@ -7,10 +7,18 @@ class Config(FlaskConfig):
 
     def from_env(self):
         """Try to overload the config with the environment."""
+
+        # ramnes: we don't want DEBUG=0 to be casted as DEBUG=True
+        def __str2bool(v):
+            return bool(int(v))
+
         env_config = {}
         for key, value in self.items():
             if key in os.environ:
-                env_config[key] = type(value)(os.environ.get(key))
+                cast = type(value)
+                if cast is bool:
+                    cast = __str2bool
+                env_config[key] = cast(os.environ.get(key))
         self.update(env_config)
         return env_config
 
