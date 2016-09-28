7e1e768e3240c83c5ff8e7dc0aa0246f48b905d2
Guillaume Gelin <ramnes@1000mercis.com>
2016-09-28 12:02:41 +0200

    Implement Currency field

--- flask_stupe/fields.py
+++ flask_stupe/fields.py
@@ -40,6 +40,48 @@ if marshmallow:
                 self.fail("invalid")
             return value
 
+    currencies = ("ADF", "ADP", "AED", "AFA", "AFN", "ALL", "AMD", "ANG", "AOA",
+                  "AOK", "AON", "AOR", "ARP", "ARS", "ATS", "AUD", "AWG", "AZM",
+                  "AZN", "BAM", "BBD", "BDT", "BEF", "BGL", "BGN", "BHD", "BIF",
+                  "BMD", "BND", "BOB", "BOP", "BOV", "BRL", "BRR", "BSD", "BTN",
+                  "BWP", "BYB", "BYN", "BYR", "BZD", "CAD", "CDF", "CHE", "CHF",
+                  "CHW", "CLF", "CLP", "CNY", "COP", "COU", "CRC", "CSD", "CSK",
+                  "CUC", "CUP", "CVE", "CYP", "CZK", "DEM", "DJF", "DKK", "DOP",
+                  "DZD", "ECS", "ECV", "EEK", "EGP", "ERN", "ESP", "ETB", "EUR",
+                  "FIM", "FJD", "FKP", "FRF", "GBP", "GEL", "GHS", "GIP", "GMD",
+                  "GNF", "GRD", "GTQ", "GWP", "GYD", "HKD", "HNL", "HRK", "HTG",
+                  "HUF", "IDR", "IEP", "ILS", "INR", "IQD", "IRR", "ISK", "ITL",
+                  "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW",
+                  "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LTL",
+                  "LUF", "LVL", "LVR", "LYD", "MAD", "MDL", "MGA", "MGF", "MKD",
+                  "MMK", "MNT", "MOP", "MRO", "MTL", "MUR", "MVR", "MWK", "MXN",
+                  "MXV", "MYR", "MZE", "MZM", "MZN", "NAD", "NGN", "NHF", "NIC",
+                  "NIO", "NLG", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PES",
+                  "PGK", "PHP", "PKR", "PLN", "PLZ", "PTE", "PYG", "QAR", "ROL",
+                  "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDD", "SDG",
+                  "SDP", "SEK", "SGD", "SHP", "SIT", "SKK", "SLL", "SML", "SOS",
+                  "SRD", "SSP", "STD", "SUB", "SUR", "SVC", "SYP", "SZL", "THB",
+                  "TJS", "TMM", "TMT", "TND", "TOP", "TPE", "TRL", "TRY", "TTD",
+                  "TWD", "TZS", "UAH", "UGX", "USD", "USN", "USS", "UYU", "UZS",
+                  "VAL", "VEB", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU",
+                  "XBA", "XBB", "XBC", "XBD", "XCD", "XDR", "XEU", "XFO", "XFU",
+                  "XOF", "XPD", "XPF", "XPT", "YER", "YUD", "YUM", "ZAR", "ZMK",
+                  "ZWD", "ZWL", "ZWR")
+
+    class Currency(marshmallow.fields.Field):
+        default_error_messages = {
+            "invalid": "Not a valid currency."
+        }
+
+        def _deserialize(self, value, attr, data):
+            try:
+                value = value.upper()
+            except AttributeError:
+                self.fail("type")
+            if value not in currencies:
+                self.fail("invalid")
+            return value
+
     class OneOf(marshmallow.fields.Field):
         default_error_messages = {
             "invalid": "Object type doesn't match any valid type"
@@ -70,7 +112,7 @@ if marshmallow:
                     pass
             self.fail("invalid")
 
-    __all__.extend(["Color", "OneOf"])
+    __all__.extend(["Color", "Currency", "OneOf"])
 
 
 if bson and marshmallow:
