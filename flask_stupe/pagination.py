0749c8dfec6d11f6438681d99ba7ee8ea64956db
Guillaume Gelin <ramnes@1000mercis.com>
2016-09-30 12:08:00 +0200

    Implement sorting with pagination

--- flask_stupe/pagination.py
+++ flask_stupe/pagination.py
@@ -9,7 +9,7 @@ __all__ = []
 
 
 if pymongo:
-    def paginate(cursor, skip=None, limit=None):
+    def paginate(cursor, skip=None, limit=None, sort=None):
         """Apply pagination to the given cursor"""
         skip = request.args.get("skip", skip, type=int)
         if skip is not None:
@@ -17,6 +17,19 @@ if pymongo:
         limit = request.args.get("limit", limit, type=int)
         if limit is not None:
             cursor = cursor.limit(limit)
+
+        sort = request.args.get("sort", sort)
+        if sort:
+            if not isinstance(sort, list):
+                sort = sort.split(",")
+            for index, item in enumerate(sort):
+                if isinstance(item, str):
+                    if item.startswith("-"):
+                        item = (item[1:], pymongo.DESCENDING)
+                        sort[index] = item
+                if not isinstance(item, tuple):
+                    sort[index] = (item, pymongo.ASCENDING)
+            cursor.sort(sort)
         return cursor
 
     __all__.append("paginate")
