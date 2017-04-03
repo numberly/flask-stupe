648d4d140d2c5f39e9f50bb9741679fcbe17344d
Guillaume Gelin <ramnes@1000mercis.com>
2017-04-03 13:10:57 +0200

    Test config

--- flask_stupe/config.py
+++ flask_stupe/config.py
@@ -1,28 +1,30 @@
+import ast
 import os
 
 from flask.config import Config as FlaskConfig
+
+
+# ramnes: we don't want DEBUG=0 to be casted as DEBUG=True
+def _str2bool(v):
+    return bool(ast.literal_eval(v))
+
+
+def _str2list(v):
+    return v.split(",")
 
 
 class Config(FlaskConfig):
 
     def from_env(self):
         """Try to overload the config with the environment"""
-
-        # ramnes: we don't want DEBUG=0 to be casted as DEBUG=True
-        def __str2bool(v):
-            return bool(int(v))
-
-        def __str2list(v):
-            return v.split(",")
-
         env_config = {}
         for key, value in self.items():
             if key in os.environ:
                 cast = type(value)
                 if cast is bool:
-                    cast = __str2bool
+                    cast = _str2bool
                 elif cast is list:
-                    cast = __str2list
+                    cast = _str2list
                 env_config[key] = cast(os.environ.get(key))
         self.update(env_config)
         return env_config
