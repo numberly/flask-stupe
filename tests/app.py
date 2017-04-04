0d46cad4cf8f2f5f0e155da41c9459a63e922d12
Guillaume Gelin <ramnes@1000mercis.com>
2017-04-04 14:16:45 +0200

    Test app.register_blueprints

--- tests/app.py
+++ tests/app.py
@@ -34,3 +34,13 @@ def test_register_converters(app, client):
     app.register_converters([FooConverter, BarConverter])
     assert app.route("/<Foo:foo>/<Bar:bar>")(lambda foo, bar: foo + bar)
     assert client.get("/foo/bar").data == b"foofoobarbar"
+
+
+def test_register_blueprints(test_apps, caplog, app):
+    import blueprintapp
+
+    app.register_blueprints(blueprintapp)
+    messages = [record.message for record in caplog.records]
+    assert " * Registering blueprint blueprintapp.apps.admin" in messages
+    assert " * Registering blueprint blueprintapp.apps.frontend" in messages
+    assert len(app.blueprints) == 2
