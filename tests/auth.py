d51f5f1525ff86eddf511f9b996882338fd77827
Guillaume Gelin <ramnes@1000mercis.com>
2017-04-13 17:12:33 +0200

    Fully test @permissions_required

--- tests/auth.py
+++ tests/auth.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_stupe.auth import auth_required, permission_required
 
 
@@ -16,14 +17,17 @@ def test_auth_required(app, client):
     assert client.get("/foo?user=1").status_code == 200
 
 
+def get_permissions():
+    permissions = request.args.get("permissions", [])
+    if permissions:
+        return permissions.split(',')
+    return []
+
+
 def test_permission_required(app, client):
     @app.before_request
     def set_user():
-        from flask import request
-        permissions = request.args.get("permissions", [])
-        if permissions:
-            permissions = permissions.split(',')
-        request.user = dict(permissions=permissions)
+        request.user = dict(permissions=get_permissions())
 
     @app.route("/foo")
     @permission_required("vip", "secret_stuff")
@@ -35,3 +39,21 @@ def test_permission_required(app, client):
     assert client.get("/foo?permissions=user,vip").status_code == 200
     assert client.get("/foo?permissions=user,secret_*").status_code == 200
     assert client.get("/foo?permissions=user,*_stuff").status_code == 200
+
+
+def test_permission_required_with_user_object(app, client):
+    class User(object):
+        pass
+
+    @app.before_request
+    def set_user():
+        request.user = User()
+        request.user.permissions = get_permissions()
+
+    @app.route("/foo")
+    @permission_required("vip", "secret_stuff")
+    def foo():
+        return "bar"
+
+    assert client.get("/foo").status_code == 403
+    assert client.get("/foo?permissions=vip").status_code == 200
