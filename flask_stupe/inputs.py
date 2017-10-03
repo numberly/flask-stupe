8f8524e1a9f997a6e057e407e777852580316068
Vincent Trubesset <vincent.trubesset@1000mercis.com>
2017-10-03 11:29:27 +0200

    Add boolean supertype

---
+++ flask_stupe/inputs.py
@@ -0,0 +1,22 @@
+
+
+# vtr: The following content belongs to https://github.com/flask-restful
+# This function is found in https://github.com/flask-restful/flask-restful/
+def boolean(value):
+    """Parse the string ``"true"`` or ``"false"`` as a boolean (case
+    insensitive). Also accepts ``"1"`` and ``"0"`` as ``True``/``False``
+    (respectively). If the input is from the request JSON body, the type is
+    already a native python boolean, and will be passed through without
+    further parsing.
+    """
+    if isinstance(value, bool):
+        return value
+
+    if not value:
+        raise ValueError("boolean type must be non-null")
+    value = value.lower()
+    if value in ('true', '1',):
+        return True
+    if value in ('false', '0',):
+        return False
+    raise ValueError("Invalid literal for boolean(): {0}".format(value))
