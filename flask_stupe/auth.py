e714265e5602431061feea51c9ac98079e510f7f
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2018-02-10 20:10:06 +0000

    Document auth module

--- flask_stupe/auth.py
+++ flask_stupe/auth.py
@@ -5,6 +5,15 @@ from flask import abort, request
 
 
 def auth_required(function):
+    """Decorator checking that the request is made by an authenticated user.
+
+    If you want to use that function, you should set a before_request handler
+    that authenticate requests when possible. It must then expose a `user`
+    attribute on the :obj:`flask.request` object.
+
+    A view decorated with :func:`auth_required` will be aborted with a status
+    code 401 if the user making the request is not authenticated.
+    """
     @functools.wraps(function)
     def __inner(*args, **kwargs):
         if not request.user:
@@ -14,6 +23,22 @@ def auth_required(function):
 
 
 def permission_required(*permissions):
+    """Decorator checking that the request is made by an user with adequate
+    privileges.
+
+    This decorator also decorate the function with the :func:`auth_required`
+    doecorator, thus the same constraints apply here.
+
+    If the :obj:`flask.request.user` is a dictionnary, it must contains a
+    "permissions" key. If it is an object, it must have "permissions"
+    attribute.
+
+    These permissions must be a list of strings. They will be checked against
+    the strings passed as parameter to this decorator. At least one must match.
+
+    You can apply this decorator multiple times if you need several permissions
+    at once to access a view.
+    """
     def __decorator(function):
         @functools.wraps(function)
         def __inner(*args, **kwargs):
