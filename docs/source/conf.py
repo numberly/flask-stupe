e6dcd3657aced15c257105fac67439dc44276a73
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2018-02-10 20:55:11 +0000

    Fix style issues in sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,11 @@ sys.path.insert(0, os.path.abspath('../..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -169,6 +171,3 @@ texinfo_documents = [
      author, 'Flask-Stupe', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
