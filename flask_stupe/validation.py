d188657d62ba09d7e4cab046d36856f167b0a0c1
Vincent <vincent.trubesset@gmail.com>
2018-04-02 10:52:10 +0200

    Remove unrequire in favor of marshmallow's partial

--- flask_stupe/validation.py
+++ flask_stupe/validation.py
@@ -11,25 +11,10 @@ except ImportError:  # pragma: no cover
 
 
 if marshmallow:
-    def unrequire(schema_or_field):
-        if isinstance(schema_or_field, marshmallow.fields.List):
-            unrequire(schema_or_field.container)
-        elif isinstance(schema_or_field, marshmallow.fields.Nested):
-            unrequire(schema_or_field.schema)
-        elif isinstance(schema_or_field, marshmallow.Schema):
-            for field in schema_or_field.fields.values():
-                unrequire(field)
-        if isinstance(schema_or_field, marshmallow.fields.Field):
-            schema_or_field.required = False
-
     class Schema(marshmallow.Schema):
 
         def __init__(self, *args, **kwargs):
-            _unrequire = kwargs.pop("unrequire", False)
             super(Schema, self).__init__(*args, **kwargs)
-
-            if _unrequire:
-                unrequire(self)
 
     def schema_required(schema):
         """Validate body of the request against the schema.
@@ -47,4 +32,4 @@ if marshmallow:
             return __inner
         return __inner
 
-    __all__.extend(["unrequire", "Schema", "schema_required"])
+    __all__.extend(["Schema", "schema_required"])
