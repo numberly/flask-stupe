73c7d7046d487c01627e9cb386d46a7866122f3e
Vincent <vincent.trubesset@gmail.com>
2018-04-02 10:52:10 +0200

    Remove unrequire in favor of marshmallow's partial

--- tests/validation.py
+++ tests/validation.py
@@ -2,48 +2,7 @@ import json
 
 from flask import request
 from flask_stupe.validation import Schema, schema_required
-from marshmallow.fields import Integer, List, Nested
-
-
-def test_unrequire():
-    class TestSchema(Schema):
-        foo = Integer(required=True)
-
-    schema = TestSchema()
-    assert schema.validate({}) == {"foo": ["Missing data for required field."]}
-
-    schema = TestSchema(unrequire=True)
-    assert schema.validate({}) == {}
-
-
-def test_unrequire_list():
-    class TestSchema(Schema):
-        foo = List(Integer, required=True)
-
-    schema = TestSchema()
-    assert schema.validate({}) == {"foo": ["Missing data for required field."]}
-
-    schema = TestSchema(unrequire=True)
-    assert schema.validate({}) == {}
-
-
-def test_unrequire_nested():
-    class NestedSchema(Schema):
-        foo = Integer(required=True)
-
-    class TestSchema(Schema):
-        foo = Nested(NestedSchema(), required=True)
-
-    schema = TestSchema()
-    assert schema.validate({}) == {
-        "foo": {
-            "foo": ["Missing data for required field."]
-        }
-    }
-
-    schema = TestSchema(unrequire=True)
-    assert schema.validate({}) == {}
-    assert schema.validate({"foo": {}}) == {}
+from marshmallow.fields import Integer
 
 
 def test_schema_required(app):
