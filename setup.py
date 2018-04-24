3ce531b075fa7098d144cff615439346148b498e
Roman Masse <roman@1000mercis.com>
2018-04-24 11:40:19 +0200

    Release 3.6.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_description():
 
 setup(
     name='Flask-Stupe',
-    version='3.6.2',
+    version='3.6.3',
     url='https://github.com/numberly/flask-stupe',
     license='MIT',
     author='Guillaume Gelin',
