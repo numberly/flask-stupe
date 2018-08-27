be7e9baa96f5c566bceab5ce93ba08f3ede205af
Roman Masse <roman@1000mercis.com>
2018-08-27 14:19:57 +0200

    Allow marshmallow version greater than 3

--- flask_stupe/validation.py
+++ flask_stupe/validation.py
@@ -8,6 +8,20 @@ __all__ = []
 
 
 if marshmallow:
+    if marshmallow.__version__.startswith('3'):  # pragma: no cover
+        def _load_schema(schema, json):
+            try:
+                return schema.load(json)
+            except marshmallow.exceptions.ValidationError as e:
+                abort(400, e.messages)
+
+    else:
+        def _load_schema(schema, json):
+            results = schema.load(json)
+            if results.errors:
+                abort(400, results.errors)
+            return results.data
+
     def schema_required(schema):
         """Validate body of the request against the schema.
 
@@ -16,10 +30,7 @@ if marshmallow:
             @functools.wraps(f)
             def __inner(*args, **kwargs):
                 json = request.get_json(force=True)
-                results = schema.load(json)
-                if results.errors:
-                    abort(400, results.errors)
-                request.schema = results.data
+                request.schema = _load_schema(schema, json)
                 return f(*args, **kwargs)
             return __inner
         return __inner
