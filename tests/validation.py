b51f7e9bbfc8afaa20620bb286e79479179b3bdf
rmasse <rambobinator@users.noreply.github.com>
2018-08-30 16:44:01 +0200

    Either schema class or instance can be used as schema_required parameter. (#23)

--- tests/validation.py
+++ tests/validation.py
@@ -1,5 +1,6 @@
 import json
 
+import pytest
 from flask import request
 from marshmallow import Schema
 from marshmallow.fields import Integer
@@ -7,12 +8,15 @@ from marshmallow.fields import Integer
 from flask_stupe.validation import schema_required
 
 
-def test_schema_required(app):
-    class TestSchema(Schema):
-        id = Integer(required=True)
+class TestSchema(Schema):
+    id = Integer(required=True)
+
+
+@pytest.mark.parametrize("TestSchema", [TestSchema, TestSchema()])
+def test_schema_required(app, TestSchema):
 
     @app.route("/foo", methods=["POST"])
-    @schema_required(TestSchema())
+    @schema_required(TestSchema)
     def foo():
         return str(request.schema.get("id"))
 
