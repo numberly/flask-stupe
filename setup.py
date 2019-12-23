d46a4b35aaac71d832362bb0645c1f231dd3102b
Guillaume Gelin <ramnes@numberly.com>
2019-12-23 14:32:40 +0100

    Release 4.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_description():
 
 setup(
     name='Flask-Stupe',
-    version='4.1.2',
+    version='4.2.0',
     url='https://github.com/numberly/flask-stupe',
     license='MIT',
     author='Guillaume Gelin',
